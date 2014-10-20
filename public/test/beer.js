a6440d930cf41631d6576da9161f0ff390a469e8
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-10-20 08:22:34 -0400

    css needs to be fixed

--- public/test/beer.js
+++ public/test/beer.js
@@ -23,6 +23,7 @@ $(document).ready(function(){
 // );
     $("div.element").on('click', function(){
         $('#beerlist').empty()
+        $('#family').empty()
         $(this).addClass('clicked')
         myVar = $(".element.clicked .content .top .left").text()
         fam_name = $(".element.clicked .content .middle").text()
@@ -32,15 +33,14 @@ $(document).ready(function(){
               $.get('/landing',formData,function(res){
             }).success(function(response){
               var res = $.parseJSON(response)
-              // console.log(res[1].name)
-              // console.log(res[2].name)
-              console.log(res[4].name)
-              console.log(res[3].name)
-              // console.log("successful ajax call")
+
+            $('#beerlist').append("<a href=beers/"+res[2].id+">"+res[2].name+"</a>")
+            $('#beerlist').append(",")
+            $('#beerlist').append("<a href=beers/"+res[3].id+">"+res[3].name+"</a>")
+            $('#beerlist').append(",")
+            $('#beerlist').append("<a href=beers/"+res[4].id+">"+res[4].name+"</a>")
             })
-        // console.log(myVar);
-        // console.log(fam_name);
-            $('#beerlist').append("<a href=families/"+myVar+">"+fam_name+"</a>")
+            $('#family').append("<a href=families/"+myVar+">"+fam_name+"</a>")
         $(this).removeClass('clicked')
     })
 })
